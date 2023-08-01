8d95b454118e8ac89dd6169c0539edae5682dec4
katness.chen <sbxew99@gmail.com>
2023-08-01 21:26:48 +0800

    describe and it

---
+++ spec/card_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../card'
+
+RSpec.describe Card do
+  card = Card.new('Ace', 'Spades')
+
+  it 'has a rank' do
+    expect(card.rank).to eq('Ace')
+  end
+
+  it 'has a suit' do
+    expect(card.suit).to eq('Spades')
+  end
+end

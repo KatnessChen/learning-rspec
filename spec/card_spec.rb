2fc70ecfc4fdbafb7d7e8bdf7798a4baff5cfc61
katness.chen <sbxew99@gmail.com>
2023-08-01 22:18:02 +0800

    benefits of let variable

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,15 +1,21 @@
 require_relative '../card'
 
 RSpec.describe Card do
-  before do
-    @card = Card.new('Ace', 'Spades')
-  end
+  # Good part of let variables
+  # 1. lazy loading - the variable is not created until it is called
+  # 2. memoization - within same "it" test suite
+  # 3. variable is not shared between examples
+  let(:card) { Card.new('Ace', 'Spades') }
+
+  # let!(:x) { 1 + 1 } # eager loading
 
   it 'has a rank' do
-    expect(@card.rank).to eq('Ace')
+    expect(card.rank).to eq('Ace')
+    card.rank = 'Queen'
+    expect(card.rank).to eq('Queen')
   end
 
   it 'has a suit' do
-    expect(@card.suit).to eq('Spades')
+    expect(card.suit).to eq('Spades')
   end
 end

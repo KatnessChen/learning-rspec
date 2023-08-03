4ca4e90b5a307f6fd2f57c985c1563b387e3a6ed
katness.chen <sbxew99@gmail.com>
2023-08-03 21:29:54 +0800

    .equal method

--- spec/equality_matchers_spec.rb
+++ spec/equality_matchers_spec.rb
@@ -13,4 +13,26 @@ RSpec.describe 'equality matchers' do
       expect(a).not_to eql(b)
     end
   end
+
+  context 'equal and be matcher' do
+    let(:c) { [1, 2, 3] }
+    let(:d) { [1, 2, 3] }
+    let(:e) { c }
+
+    it 'cares about object identity' do
+      # check same value
+      expect(c).to eq(e)
+      expect(c).to eq(d)
+      expect(c).to eq([1, 2, 3])
+
+      # check same identity
+      expect(c).to equal(e)
+      expect(c).not_to equal([1, 2, 3])
+      expect(c).to be(e) # alias method for `equality`
+
+      # same value but different identity
+      expect(c).not_to equal(d)
+      expect(c).not_to equal([1, 2, 3])
+    end
+  end
 end
